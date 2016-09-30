8c8c08a2d1686aaa745f5fd29376b2c4ce67231c
divinites <divinites@gmail.com>
2016-09-30 15:46:47 +0100

    change 'gissue' to 'GitHubIssue'

--- libgit/utils.py
+++ libgit/utils.py
@@ -187,7 +187,7 @@ def create_new_issue_view():
     view = sublime.active_window().new_file()
     view.run_command("set_file_type",
                       {"syntax":
-                       "Packages/gissues/issue.tmLanguage"})
+                       "Packages/GitHubIssue/issue.tmLanguage"})
     view.run_command("insert_issue", {"issue": snippet})
     view.set_scratch(True)
 
